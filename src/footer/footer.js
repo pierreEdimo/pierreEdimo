a4deb228a6e6e5287ead14504908f6bf0ee97c71
pierre edimo <pierredimo@live.com>
2021-01-17 06:54:43 +0100

    i added the footer

---
+++ src/footer/footer.js
@@ -0,0 +1,13 @@
+import './footer.css'; 
+
+function Footer (){
+    return (
+        <footer className="app-footer">
+        <a href="www.facebook.com" className="outline-button-grey" >Contact Me!</a>
+        <a href="http://www.freepik.com">Image designed by rawpixel.com / Freepik</a>
+        <p> website coded by mysefl</p>
+    </footer>
+    ); 
+}
+
+export default Footer; 
